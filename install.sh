6c98859426501e956197cff324901cb8a0612876
Shyim <github@shyim.de>
2022-10-26 09:13:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ curl -sS https://starship.rs/install.sh | sh -s -- -f
 cp ~/.dotfiles/.gitconfig ~/.gitconfig
 cp ~/.dotfiles/.wakatime.cfg ~/.wakatime.cfg
 cp ~/.dotfiles/.bashrc.d/* ~/.bashrc.d/
+
+mkdir -p ~/.ssh && touch ~/.ssh/known_hosts
+ssh-keyscan github.com >> ~/.ssh/known_hosts
