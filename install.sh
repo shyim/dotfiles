1c3b8f22e237f610ecef03575c5eec3759de94cb
Shyim <github@shyim.de>
2022-10-27 22:55:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,14 @@ cp ~/.dotfiles/.bashrc.d/* ~/.bashrc.d/
 
 mkdir -p ~/.ssh && touch ~/.ssh/known_hosts
 ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+# Setup neovim
+
+sudo apt purge -y neovim*
+
+wget https://github.com/neovim/neovim/releases/download/v0.8.0/nvim-linux64.deb
+sudo dpkg -i nvim-linux64.deb
+rm nvim-linux64.deb
+
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+
