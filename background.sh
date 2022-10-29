b436e72aff61a4f0a437b9fe73fa203986a4dd60
Shyim <github@shyim.de>
2022-10-30 01:57:05 +0200

    uff

--- background.sh
+++ background.sh
@@ -17,5 +17,7 @@ if [[ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]]; then
     fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher'
     fish -c 'fisher install PatrickF1/fzf.fish'
 
+    rm -rf "$HOME/.local/share/fish/fish_history" && rm /workspace/.fish_history && ln -s "$HOME/.local/share/fish/fish_history" /workspace/.fish_history && touch /workspace/.fish_history
+
     bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
 fi
