35f1f69ef50b461773d4dba705ec6feb221676ea
Shyim <github@shyim.de>
2022-10-30 01:56:09 +0200

    test

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ then
     echo "Setting up Fish"
     # Setup a History forwarder to save fish config between runs
     if [[ -d /workspace ]]; then
-        rm -f "$HOME/.local/share/fish/fish_history" /workspace/.fish_history && ln -s "$HOME/.local/share/fish/fish_history" /workspace/.fish_history
+        rm -rf "$HOME/.local/share/fish/fish_history" && rm /workspace/.fish_history && ln -s "$HOME/.local/share/fish/fish_history" /workspace/.fish_history && touch /workspace/.fish_history
         
         echo "Configured history forwarder"
     fi
