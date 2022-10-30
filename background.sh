7dcfa00ca5022548bb9bb6cf4cc69cf728467673
Shyim <github@shyim.de>
2022-10-30 02:12:37 +0200

    install nix if missing

--- background.sh
+++ background.sh
@@ -5,20 +5,23 @@
 mkdir -p ~/.ssh && sudo rm -f ~/.ssh/known_hosts
 ssh-keyscan github.com > ~/.ssh/known_hosts
 
-if [[ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]]; then
-    . "$HOME/.nix-profile/etc/profile.d/nix.sh"
-
-    nix-env -iA nixpkgs.neovim nixpkgs.ripgrep nixpkgs.fd nixpkgs.fzf nixpkgs.fish nixpkgs.starship
-
-    sudo chsh -s "$HOME/.nix-profile/bin/fish" "$USER"
-
-    sudo apt purge -y neovim* fish*
-
-    fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher'
-    fish -c 'fisher install PatrickF1/fzf.fish'
-
-    rm -f "$HOME/.local/share/fish/fish_history" && ln -s /workspace/.fish_history "$HOME/.local/share/fish/fish_history"
-    sudo cp "$HOME/.nix-profile/bin/fish" /usr/bin/fish
-
-    bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
+if [[ ! -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]]; then
+    sh <(curl -L https://nixos.org/nix/install) --no-daemon
 fi
+
+
+. "$HOME/.nix-profile/etc/profile.d/nix.sh"
+
+nix-env -iA nixpkgs.neovim nixpkgs.ripgrep nixpkgs.fd nixpkgs.fzf nixpkgs.fish nixpkgs.starship
+
+sudo chsh -s "$HOME/.nix-profile/bin/fish" "$USER"
+
+sudo apt purge -y neovim* fish*
+
+fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher'
+fish -c 'fisher install PatrickF1/fzf.fish'
+
+rm -f "$HOME/.local/share/fish/fish_history" && ln -s /workspace/.fish_history "$HOME/.local/share/fish/fish_history"
+sudo cp "$HOME/.nix-profile/bin/fish" /usr/bin/fish
+
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) -y
