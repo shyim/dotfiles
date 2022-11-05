5a0e4babc910b540082a3b878a45a03f4a340e39
Shyim <github@shyim.de>
2022-11-05 23:31:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
-cp ~/.dotfiles/.wakatime.cfg ~/.wakatime.cfg
-cp ~/.dotfiles/.bashrc.d/* ~/.bashrc.d/
-cp ~/.dotfiles/.gitconfig ~/.gitconfig
-cp -r ~/.dotfiles/.config/* ~/.config/
+dotfiles_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+workspace_dir="/workspaces"
+
+
+cp $dotfiles_dir/.wakatime.cfg ~/.wakatime.cfg
+cp $dotfiles_dir/.bashrc.d/* ~/.bashrc.d/
+cp -r $dotfiles_dir/.config/* ~/.config/
+
+
 
 if [[ "$USER" == "gitpod" ]]; then
+    workspace_dir="/workspace"
+
+    # Only on Gitpod is Git Config annoying
+    cp $dotfiles_dir/.gitconfig ~/.gitconfig
+
     echo "[credential]" >> ~/.gitconfig
     echo "    helper = /usr/bin/gp credential-helper" >> ~/.gitconfig
 fi
@@ -14,12 +24,12 @@ if command -v fish &> /dev/null
 then
     echo "Setting up Fish"
 
-    touch /workspace/.fish_history
+    touch "${workspace_dir}/.fish_history"
 
     # Setup a History forwarder to save fish config between runs
     if [[ -d /workspace ]]; then
         mkdir -p "$HOME/.local/share/fish"
-        rm -f "$HOME/.local/share/fish/fish_history" && ln -s /workspace/.fish_history "$HOME/.local/share/fish/fish_history"
+        rm -f "$HOME/.local/share/fish/fish_history" && ln -s "${workspace_dir}/.fish_history" "$HOME/.local/share/fish/fish_history"
         
         echo "Configured history forwarder"
     fi
@@ -27,4 +37,7 @@ then
     sudo chsh -s $(which fish) "$USER"
 fi
 
-nohup bash ~/.dotfiles/background.sh &
+export dotfiles_dir
+export workspace_dir
+
+nohup bash "$dotfiles_dir/background.sh" &
