48ffe303f8e2f700b1974870cb92d2854280c68a
Shyim <github@shyim.de>
2022-11-29 12:03:12 +0100

    Update fleet.sh

--- fleet.sh
+++ fleet.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -LSs "https://download.jetbrains.com/product?code=FLL&release.type=eap&platform=linux_x64" --output /tmp/fleet && chmod +x /tmp/fleet
+curl -LSs "https://download.jetbrains.com/product?code=FLL&release.type=preview&release.type=eap&platform=linux_x64" --output /tmp/fleet && chmod +x /tmp/fleet
 /tmp/fleet launch workspace -- --auth=accept-everyone --publish --enableSmartMode "--projectDir=${GITPOD_REPO_ROOT}"
